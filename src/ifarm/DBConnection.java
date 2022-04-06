15d21944d310aed49a1bd32759e427fc129cdd96
Lee Quan <leequan1101@gmail.com>
2022-04-06 22:45:57 +0800

    undo normalization/polymorphic table

--- src/ifarm/DBConnection.java
+++ src/ifarm/DBConnection.java
@@ -14,28 +14,17 @@ import javax.swing.JOptionPane;
  * @author Lee Quan
  */
 public class DBConnection {
-//        static Connection conn=null ;
-//    public static Connection ConnectDB(){
-//        try{
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ifarm?serverTimezone=UTC","root","1234");
-//            return conn;
-//        } catch (Exception e){
-//            JOptionPane.showMessageDialog(null,e);
-//        }
-//        return conn;
-//    }
 
     public static void main(String[] args) {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ifarm?serverTimezone=UTC","root","1111");
-//            Statement myStmt = conn.createStatement();
-//            ResultSet rs = myStmt.executeQuery("SELECT * FROM farm");
-//            
-//            
-//            while (rs.next()){
-//                System.out.println(rs.getString("name") + " is name, and address is " + rs.getString("address"));
-//            }
+            Statement myStmt = conn.createStatement();
+            ResultSet rs = myStmt.executeQuery("SELECT * FROM farm");
+            
+            
+            while (rs.next()){
+                System.out.println(rs.getString("name") + " is name, and address is " + rs.getString("address"));
+            }
 //        try {
 //            conn = DriverManager.getConnection("jdbc:mysql://localhost/ifarm?"
 //                    + "user=root&password=1111");
