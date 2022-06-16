18d3ea8c1f0e5690624574edf1d36efa27706636
wklim2010 <69759113+wklim2010@users.noreply.github.com>
2022-06-17 07:56:04 +0800

    Update DBConnection.java

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -44,7 +44,10 @@ public class DBConnection {
         pstmt.execute();
     }
 
-    
+    public void truncate(String sql) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(sql);
+    }
 
     public void update(String sql) throws SQLException {
         Statement stmt = conn.createStatement();
